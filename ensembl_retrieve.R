a6b1a085931df83422a62e41e10025ce1b5f18d9
vFlav <flv691@mail.usask.ca>
2018-10-05 13:31:39 -0600

    Adjust documentation of EnsEMBL retrieve script

--- ensembl_retrieve.R
+++ ensembl_retrieve.R
@@ -1,38 +1,33 @@
 #!/usr/bin/env Rscript
 
-# An R script that takes as standard input EnsEMBL IDs, start and end sites and 
+# Takes as standard input EnsEMBL IDs, start and end sites and 
 # chromosome numbers. Once standard input is detected, it retrieves sequences
 # in FASTA format from EnsEMBL using BiomaRt and writes them to standard output.
 
-#---------------------------------------------------------------------------------------
 library(biomaRt)
-#---------------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------------------------
-mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl") # connect to BioMart db
-input <- file("stdin", open = "r") # stdin
-#---------------------------------------------------------------------------------------
+# establish BioMart DB connection
+# use Homo Sapiens as the dataset
+mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl") 
+input <- file("stdin", open = "r")
 
-#---------------------------------------------------------------------------------------
 current.line <- 1 
 
 while(length(line <- readLines(input, n=1)) > 0){	
 
 	result.list <- list()
 
-	result.list[[current.line]] <- strsplit(line, split="\t") # separate input
+	result.list[[current.line]] <- strsplit(line, split="\t")
 
-	M <- unlist(result.list) # unlist the line so elements are accessible
+	M <- unlist(result.list)
 
 	# M[1] EnsEMBL ID, M[2] start, M[3] end, M[4] chromosome number
-	
 	seq <- getSequence(id=M[1], 
-					   type="ensembl_gene_id", 
-					   seqType="gene_flank", 
-					   upstream=1000, 
-					   mart=mart)
+			   type="ensembl_gene_id", 
+			   seqType="gene_flank", 
+			   upstream=1000, 
+			   mart=mart)
 	show(seq)
-
 	current.line <- current.line + 1
 }
 
