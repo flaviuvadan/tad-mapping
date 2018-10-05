59b58c74a167961275283ee9eeaeb38da7a404d0
vFlav <flv691@mail.usask.ca>
2018-10-05 13:27:24 -0600

    Adjust documentation of the EnsEMBL format script

--- ensembl_format.pl
+++ ensembl_format.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# PErl script that takes in a list of tab delimited chromosome locations 
+# Takes in a list of tab delimited chromosome locations 
 # with chromosomal annotations, e.g. chr1 0 150, and converts it to the
 # Ensembl's BioMart format, e.g. chromosome:start:end:str
 
@@ -9,5 +9,7 @@ use strict;
 
 foreach my $line (<>){
 	my @result =  split(/\s/, $line);
+	# we take 1000 to the left and 50 to the right of the chromosome
+	# for future promoter localization using Clover and MEME-ChIP
 	print $result[0]."\t".($result[1]-1000)."\t".($result[1]+50)."\t".$result[3]."\n";
 }
